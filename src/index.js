4b4106c53a6555ba1b09c105bdd731f950f93cea
Max Kovalenko <tutmrmaxi@gmail.com>
2021-08-16 20:34:12 +0300

    feat: add reverse num

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n > 0) return +(n.toString().split('').reverse().join(''));
+    if (n < 0) return (Math.abs(n)).toString().split('').reverse().join('');
 }
